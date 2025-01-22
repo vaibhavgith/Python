449196a44fc51ddde4dd9f2f01be1f62a3e21bed
VaibhavNevase <137980129+vaibhavgith@users.noreply.github.com>
2025-01-22 10:23:27 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,25 @@
+# ===================== String =====================
+# ch = 
+# if 'a' <= ch <='z' or 'A' <= ch <= 'Z':
+#     print("Char")
+# else:
+#     print("No")
+
+# ch = 'd'
+# asci = ord(ch)
+# print(asci)
+# ================================================
+ch = 'Vaibhav'
+# count =0
+# for i in ch :
+#     count+=1
+# print(count)
+# print(len(ch))
+# print(ch.swapcase())
+
+# revser = ch[::-1]
+# if revser == ch :
+#     print("Yes")
+# else:
+#     print("No")
+
